33ad5a47cf24fc759304adc25f028c51ff9c462e
shyam <shyamanandapadmanaban@gmail.com>
2026-07-24 21:07:36 +0530

    added train.py

---
+++ train.py
@@ -0,0 +1,33 @@
+import pandas
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+print("Loading dataset")
+CSV = "indian_job_listings_dataset.csv"
+df['combined'] = (
+    df['title'].fillna('')+ ' ' +
+    df['company_name'].fillna('')+ ' ' +
+    df['location'].fillna('')+ ' ' +
+    df['description'].fillna('')+ ' ' +
+    df['requirements'].fillna('')+ ' ' +
+    df['salary_range'].fillna('')+ ' ' +
+    df['employment_type'].fillna('')
+)
+X = df['combined']
+Y = df['fraudulent']
+vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1,2))
+X_vectorized = vectorizer.fit_transform(X)
+X_train, X_test, Y_train, Y_test = train_test_split(X_vectorized, Y, train_size=0.2, random_state=42, stratify=Y)
+print("Loading Model")
+model = LogisticRegression(max_iter=1000, class_weight='balanced')
+model.fit(X_train, Y_train)
+Y_pred = model.predict(X_test)
+acc = accuracy_score(Y_pred,Y_test)
+print("Accuracy Score: {100*acc:.2f}%")
+print("\n Classification Report")
+print(classification_report(Y_test, Y_pred, target_names=["REAL JOB", "FAKE JOB"])
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
+pickle.dump(model, open("model.pkl", "wb"))
+print("model, vectorizer saved as pkl to avoid retraining")
