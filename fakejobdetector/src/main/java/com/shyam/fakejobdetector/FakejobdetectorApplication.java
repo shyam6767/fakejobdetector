41467b6079bc68bbc7a7ddb9e98004ff17664813
shyam <shyamanandapadmanaban@gmail.com>
2026-07-26 01:19:56 +0530

    added springboot, and application.properties

---
+++ fakejobdetector/src/main/java/com/shyam/fakejobdetector/FakejobdetectorApplication.java
@@ -0,0 +1,13 @@
+package com.shyam.fakejobdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakejobdetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakejobdetectorApplication.class, args);
+	}
+
+}
---
+++ fakejobdetector/src/test/java/com/shyam/fakejobdetector/FakejobdetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyam.fakejobdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakejobdetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
