63e53a11d2d040aec59bba751bf65b3367be6505
shyam <shyamanandapadmanaban@gmail.com>
2026-07-30 00:22:48 +0530

    url updated

--- fakejobdetector/src/main/java/com/shyam/fakejobdetector/service/JobService.java
+++ fakejobdetector/src/main/java/com/shyam/fakejobdetector/service/JobService.java
@@ -15,7 +15,7 @@ public class JobService {
     @Autowired
     private JobRepository jobRepository;
 
-    private final String FLASK_URL = "http://localhost:5000/predict";
+    private final String FLASK_URL = "https://fakejobdetector-fa3d.onrender.com";
 
     public String analyzeJob(String description) {
         // call Flask API
