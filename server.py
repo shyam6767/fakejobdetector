e6b622220894403ec61c2c3411e50cfb715ce29e
shyam <shyamanandapadmanaban@gmail.com>
2026-07-29 20:10:37 +0530

    server.py changed for cloud operations than local

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,4 @@ def predict():
     return jsonify({"result": result})
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
